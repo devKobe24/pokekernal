c5b3194844bc75d22f17074de321771d9149401a
devKobe24 <dev.skyachieve91@gmail.com>
2025-12-20 07:51:35 +0900

    feat(entity): BaseTimeEntity 생성 및 JPA Auditing 기능 구현
    - JPA Auditing을 위한 BaseTimeEntity 추가
    - @CreatedDate, @LastModifiedDate를 통한 생성/수정 시간 자동 관리
    - @MappedSuperclass로 다른 엔티티에서 상속 가능하도록 구성
    - AuditingEntityListener를 통한 Auditing 기능 활성화

---
+++ src/main/java/com/kobe/pokekernle/global/entity/BaseTimeEntity.java
@@ -0,0 +1,33 @@
+package com.kobe.pokekernle.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.kobe.pokekernle.global.entity
+ * fileName       : BaseTimeEntity
+ * author         : kobe
+ * date           : 2025. 12. 20.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 12. 20.        kobe       최초 생성
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+}
