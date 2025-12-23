b9ebfbdc93a8476a8446ac6493d033637e7cabb1
devKobe24 <dev.skyachieve91@gmail.com>
2025-12-24 03:47:53 +0900

    feat: pokekernel 0.0.6 버전 배포

--- src/main/java/com/kobe/pokekernle/domain/admin/service/ImageUploadService.java
+++ src/main/java/com/kobe/pokekernle/domain/admin/service/ImageUploadService.java
@@ -34,6 +34,12 @@ public class ImageUploadService {
     @Value("${spring.cloud.aws.s3.bucket:}")
     private String bucketName;
 
+    @Value("${spring.cloud.aws.region.static:ap-northeast-2}")
+    private String region;
+
+    @Value("${app.s3.cloudfront-domain:}")
+    private String cloudFrontDomain;
+
     private boolean useS3;
 
     @Autowired
@@ -61,8 +67,21 @@ public class ImageUploadService {
             // S3로 업로드 (운영 환경)
             String s3Key = "images/" + fileName;
             s3Template.upload(bucketName, s3Key, file.getInputStream());
-            String fileUrl = s3Template.download(bucketName, s3Key).getURL().toString();
+            
+            // S3 Public URL 생성
+            // CloudFront 도메인이 설정되어 있으면 사용, 없으면 S3 직접 Public URL 사용
+            String fileUrl;
+            if (cloudFrontDomain != null && !cloudFrontDomain.isEmpty()) {
+                // CloudFront를 통한 URL (권장)
+                fileUrl = "https://" + cloudFrontDomain + "/" + s3Key;
+            } else {
+                // S3 직접 Public URL (버킷이 public이어야 함)
+                // 형식: https://{bucket-name}.s3.{region}.amazonaws.com/{key}
+                fileUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, s3Key);
+            }
+            
             log.info("[S3 UPLOAD] 완료: {}", fileUrl);
+            log.info("[S3 UPLOAD] Bucket: {}, Key: {}, Region: {}", bucketName, s3Key, region);
             return fileUrl;
         } else {
             // 로컬 파일 시스템에 저장 (개발 환경)
