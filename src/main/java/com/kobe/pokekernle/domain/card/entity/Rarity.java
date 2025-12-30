35b9d0ac7c297047862f40c0826f6069155f6600
devKobe24 <dev.skyachieve91@gmail.com>
2025-12-31 05:47:51 +0900

    feat: 레어도 추가 및 수정

--- src/main/java/com/kobe/pokekernle/domain/card/entity/Rarity.java
+++ src/main/java/com/kobe/pokekernle/domain/card/entity/Rarity.java
@@ -12,15 +12,22 @@ package com.kobe.pokekernle.domain.card.entity;
  * 2025. 12. 20.        kobe       최초 생성
  */
 public enum Rarity {
-    COMMON,             // 일반
-    UNCOMMON,           // 언커먼
-    RARE,               // 레어 (일반 레어)
-    HOLO_RARE,          // 홀로 레어(반짝이는 레어)
-    DOUBLE_RARE,        // RR (Double Rare) - V, EX 등
-    ULTRA_RARE,         // UR/SR (Full Art 등)
-    ILLUSTRATION_RARE,  // AR/SAR (일러스트 강조)
-    SECRET_RARE,        // 시크릿 레어 (번호가 101/100 처럼 넘어가는 것)
-    PROMO,              // 프로모
-    UNKNOWN             // 알 수 없음 (매핑 실패 시)
+    MIRROR_POKE_BALL,               // 미러볼(포켓볼 버전)
+    MIRROR_MASTER_BALL,             // 미러볼(마스터볼 버전)
+    COMMON,                         // 일반
+    UNCOMMON,                       // 언커먼
+    RARE,                           // 레어 (일반 레어)
+    DOUBLE_RARE,                    // RR (Double Rare) - V, EX 등
+    SUPER_RARE,                     // SR (Super Rare)
+    ART_RARE,                       // AR (Art Rare)
+    SPECIAL_ART_RARE,               // SAR
+    ULTRA_RARE,                     // UR
+    ACE,                            // ACE SPEC (에이스 스펙)
+    SHINY,                          // S (Shiny, 색이 다른 포켓몬(이로치)을 위한 레어도)
+    SHINING_SUPER_RARE,             // SSR (샤이닝 슈퍼레어, 색이 다른 GX, V, VMAX, ex 포켓몬의 등급)
+    BLACK_WHITE_RARE,               // BWR (블랙 화이트 레어, 확장팩 블랙 볼트, 화이트 플레어 전용등급)
+    MEGA_ULTRA_RARE,                // MUR (메가 울트라 레어, 메가브레이브, 메가심포니아부터 추가된 등급)
+    MEGA_ATTACK_RARE,               // MAR (메가 어택 레어, MEGA드림ex 부터 추가된 등급)
+    PROMO                           // PROMO 카드를 통해 얻은 등급
     // 필요에 따라 계속 추가
 }
